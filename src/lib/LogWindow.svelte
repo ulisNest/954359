2a215cdbd1e0d804ada722584defb172cb1445bb
ullisNest <morsello.1851339@studenti.uniroma1.it>
2022-08-06 14:44:05 +0200

    first commit

---
+++ src/lib/LogWindow.svelte
@@ -0,0 +1,17 @@
+<script>
+  // your script goes here
+</script>
+
+<section class="logWindow">
+  <span> helo</span>
+</section>
+
+<style>
+    section.logWindow {
+        outline: 1px solid gray;
+        height: 65vh;
+        border-radius: 1%;
+        margin-bottom: 1%;
+        text-align: left;
+    }
+</style>
